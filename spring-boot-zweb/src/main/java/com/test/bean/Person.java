1fc11787cbd2fc170aed1de09795bb147b50ee17
jy00884666 <87419590@qq.com>
2022-09-03 17:54:43 +0800

    20220903

--- spring-boot-zweb/src/main/java/com/test/bean/Person.java
+++ spring-boot-zweb/src/main/java/com/test/bean/Person.java
@@ -1,36 +1,16 @@
 package com.test.bean;
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 /*把普通pojo实例化到spring容器中,相当于配置文件中的 <bean id="" class=""/> */
 @Component
+@Data
 public class Person {
 	
 	private String name;
 	
 	private String sex;
 	
-	public String getName() {
-		return name;
-	}
 	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getSex() {
-		return sex;
-	}
-	
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-	
-	@Override
-	public String toString() {
-		return "Person{" +
-				"name='" + name + '\'' +
-				", sex='" + sex + '\'' +
-				'}';
-	}
 }
